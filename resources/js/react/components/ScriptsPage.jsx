3a032d330f41c2f6a7e94fc7f64dcd0feb5f825d
Bernard Polidario <bernard@weeklyhow.com>
2022-03-07 12:37:18 +0800

    Add Script Tags to Shopify Stores

--- resources/js/react/components/ScriptsPage.jsx
+++ resources/js/react/components/ScriptsPage.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import {gql, useMutation} from '@apollo/client';
 
 function ScriptsPage() {
+    const CREATE_SCRIPTTAG_QUERY = gql`mutation {
+        scriptTagCreate(input: {
+            cache: false,
+            displayScope: ALL,
+            src: "https://weeklyhow.com/codes/larabelle/script.js"
+        }) {
+            scriptTag {
+                id
+                src
+                displayScope
+            }
+        }
+    }`;
+
+    const [createScriptTagMutation, { data }] = useMutation(CREATE_SCRIPTTAG_QUERY);
+
+    createScriptTagMutation();
+
     return <div>This is the scripts page</div>;
 }
 
