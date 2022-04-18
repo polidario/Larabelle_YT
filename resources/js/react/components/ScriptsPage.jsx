16347a91067b74d3d794934c06a590cd8c831695
Bernard Polidario <bernard@weeklyhow.com>
2022-04-18 23:16:10 +0800

    Check if Script Tag is installed

--- resources/js/react/components/ScriptsPage.jsx
+++ resources/js/react/components/ScriptsPage.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
-import {gql, useMutation} from '@apollo/client';
+import {gql, useMutation, useQuery} from '@apollo/client';
+import { Button } from '@shopify/polaris';
+import { useState } from 'react';
 
 function ScriptsPage() {
+    const [isInstalled, setInstalled] = useState(false);
+    const SCRIPT_TAG_URL = "https://weeklyhow.com/codes/larabelle/script.js";
+
+    const CHECK_SCRIPT_TAG = gql`query {
+        scriptTags(first: 50 src: "${SCRIPT_TAG_URL}") {
+            edges {
+                node {
+                    id
+                    src
+                }
+            }
+        }
+    }`;
+
+    const { loading, error, data } = useQuery(CHECK_SCRIPT_TAG);
+
+    if(error) return `Error: ${error.message}`;
+    if(data != undefined) {
+        if(data.scriptTags.edges[0].node.src == SCRIPT_TAG_URL && !isInstalled) {
+            setInstalled(true);
+        }
+    }
+
     const CREATE_SCRIPTTAG_QUERY = gql`mutation {
         scriptTagCreate(input: {
             cache: false,
             displayScope: ALL,
-            src: "https://weeklyhow.com/codes/larabelle/script.js"
+            src: "${SCRIPT_TAG_URL}"
         }) {
             scriptTag {
                 id
@@ -16,11 +41,19 @@ function ScriptsPage() {
         }
     }`;
 
-    const [createScriptTagMutation, { data }] = useMutation(CREATE_SCRIPTTAG_QUERY);
+    const [createScriptTagMutation, { createScriptTagMutationData }] = useMutation(CREATE_SCRIPTTAG_QUERY);
 
-    createScriptTagMutation();
-
-    return <div>This is the scripts page</div>;
+    return <>
+        <div>
+            <Button
+                onClick={createScriptTagMutation}
+                disabled={ isInstalled ? true : false }
+                loading={ loading ? true : false }
+            >
+                Install Script Tag
+            </Button>
+        </div>
+    </>;
 }
 
 export default ScriptsPage;
